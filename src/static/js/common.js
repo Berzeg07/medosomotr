11c221e02def77ec1cb042315ccc1caf31a32abe
Aslan <berezg86@gmail.com>
2020-11-11 15:01:29 +0300

    Верстка блоков до футера

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,33 @@
 $(document).ready(function () {
-    // alert(1);
+    var contSlider = new Swiper('.types-gallery', {
+        slidesPerView: 4,
+        spaceBetween: 0,
+        loop: true,
+        observer: true,
+        observeParents: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            499: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            }
+        }
+    });
+
+    $('.selectcustom select').selectric();
 });
